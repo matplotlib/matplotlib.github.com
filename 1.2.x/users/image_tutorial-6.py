59724fa971bde502b19692ce640641317e003148
Michael Droettboom <mdboom@gmail.com>
2012-09-12 10:55:32 -0400

    Updating 1.2.0rc1 docs

--- 1.2.x/users/image_tutorial-6.py
+++
@@ -1,6 +0,0 @@
-import matplotlib.pyplot as plt
-import matplotlib.image as mpimg
-import numpy as np
-img = mpimg.imread('../_static/stinkbug.png')
-lum_img = img[:,:,0]
-plt.hist(lum_img.flatten(), 256, range=(0.0,1.0), fc='black', ec='black')
--- 1.2.x/users/legend_guide-1.py
+++
@@ -1,6 +0,0 @@
-z = np.random.randn(10)
-
-p1a, = plt.plot(z, "ro", ms=10, mfc="r", mew=2, mec="r") # red filled circle
-p1b, = plt.plot(z[:5], "w+", ms=10, mec="w", mew=2) # white cross
-
-plt.legend([p1a, (p1a, p1b)], ["Attr A", "Attr A+B"])
