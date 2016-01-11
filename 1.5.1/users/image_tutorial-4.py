f222e38d05d1aa9110c61e29a5aa404803541585
Thomas A Caswell <tcaswell@gmail.com>
2016-01-10 23:19:02 -0500

    Updated docs for 1.5.1

---
+++ 1.5.1/users/image_tutorial-4.py
@@ -0,0 +1,7 @@
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import numpy as np
+img = mpimg.imread('../_static/stinkbug.png')
+lum_img = img[:, :, 0]
+imgplot = plt.imshow(lum_img)
+imgplot.set_cmap('spectral')
