753a8e07c410054f8260e3db1f7a9697388fe362
Kristen M. Thyng <kthyng@gmail.com>
2016-07-17 08:16:07 -0400

    update to cmocean docs with new example

--- cmocean/index-7.py
+++ cmocean/index-7.py
@@ -3,6 +3,6 @@ import matplotlib.pyplot as plt
 
 fig = plt.figure(figsize=(8, 3))
 ax = fig.add_subplot(1, 2, 1)
-cmocean.plots.test(cmocean.cm.amp, ax=ax)
+cmocean.plots.test(cmocean.cm.tempo, ax=ax)
 ax = fig.add_subplot(1, 2, 2)
-cmocean.plots.quick_plot(cmocean.cm.amp, ax=ax)
+cmocean.plots.quick_plot(cmocean.cm.tempo, ax=ax)
