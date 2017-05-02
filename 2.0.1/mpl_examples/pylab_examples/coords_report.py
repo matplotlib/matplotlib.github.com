572f8273ba876060e2e90fd728733a0f0442a013
Thomas A Caswell <tcaswell@gmail.com>
2017-05-02 14:44:02 -0400

    rebuilding docs for 2.0.1

---
+++ 2.0.1/mpl_examples/pylab_examples/coords_report.py
@@ -0,0 +1,17 @@
+# override the default reporting of coords
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def millions(x):
+    return '$%1.1fM' % (x*1e-6)
+
+x = np.random.rand(20)
+y = 1e7*np.random.rand(20)
+
+fig, ax = plt.subplots()
+ax.fmt_ydata = millions
+plt.plot(x, y, 'o')
+
+plt.show()
