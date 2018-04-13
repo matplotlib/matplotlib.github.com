63e803e87a1004cdbbf372e3c50e2b5c3160177e
Thomas A Caswell <tcaswell@gmail.com>
2018-04-12 23:27:31 -0400

    rebuild the docs for 2.2.2-doc updates

--- _downloads/date_index_formatter.py
+++ _downloads/date_index_formatter.py
@@ -1,49 +1,48 @@
 """
-====================
-Date Index Formatter
-====================
+=====================================
+Custom tick formatter for time series
+=====================================
 
-When plotting daily data, a frequent request is to plot the data
-ignoring skips, e.g., no extra spaces for weekends.  This is particularly
-common in financial time series, when you may have data for M-F and
-not Sat, Sun and you don't want gaps in the x axis.  The approach is
-to simply use the integer index for the xdata and a custom tick
-Formatter to get the appropriate date string for a given index.
+When plotting time series, e.g., financial time series, one often wants
+to leave out days on which there is no data, i.e. weekends.  The example
+below shows how to use an 'index formatter' to achieve the desired plot
 """
-
 from __future__ import print_function
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 import matplotlib.cbook as cbook
-from matplotlib.dates import bytespdate2num, num2date
-from matplotlib.ticker import Formatter
+import matplotlib.ticker as ticker
 
+# Load a numpy record array from yahoo csv data with fields date, open, close,
+# volume, adj_close from the mpl-data/example directory. The record array
+# stores the date as an np.datetime64 with a day unit ('D') in the date column.
+with cbook.get_sample_data('goog.npz') as datafile:
+    r = np.load(datafile)['price_data'].view(np.recarray)
+r = r[-30:]  # get the last 30 days
+# Matplotlib works better with datetime.datetime than np.datetime64, but the
+# latter is more portable.
+date = r.date.astype('O')
 
-datafile = cbook.get_sample_data('msft.csv', asfileobj=False)
-print('loading %s' % datafile)
-msft_data = np.genfromtxt(datafile, delimiter=',', names=True,
-                          converters={0: bytespdate2num('%d-%b-%y')})[-40:]
-
-
-class MyFormatter(Formatter):
-    def __init__(self, dates, fmt='%Y-%m-%d'):
-        self.dates = dates
-        self.fmt = fmt
-
-    def __call__(self, x, pos=0):
-        'Return the label for time x at position pos'
-        ind = int(np.round(x))
-        if ind >= len(self.dates) or ind < 0:
-            return ''
-
-        return num2date(self.dates[ind]).strftime(self.fmt)
-
-formatter = MyFormatter(msft_data['Date'])
-
-fig, ax = plt.subplots()
-ax.xaxis.set_major_formatter(formatter)
-ax.plot(np.arange(len(msft_data)), msft_data['Close'], 'o-')
+# first we'll do it the default way, with gaps on weekends
+fig, axes = plt.subplots(ncols=2, figsize=(8, 4))
+ax = axes[0]
+ax.plot(date, r.adj_close, 'o-')
+ax.set_title("Default")
 fig.autofmt_xdate()
+
+# next we'll write a custom formatter
+N = len(r)
+ind = np.arange(N)  # the evenly spaced plot indices
+
+
+def format_date(x, pos=None):
+    thisind = np.clip(int(x + 0.5), 0, N - 1)
+    return date[thisind].strftime('%Y-%m-%d')
+
+ax = axes[1]
+ax.plot(ind, r.adj_close, 'o-')
+ax.xaxis.set_major_formatter(ticker.FuncFormatter(format_date))
+ax.set_title("Custom tick formatter")
+fig.autofmt_xdate()
+
 plt.show()
