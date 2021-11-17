4db3a0792dad7ade9876fcdd99384216b07973a0
Thomas A Caswell <tcaswell@gmail.com>
2021-11-17 10:41:44 -0500

    Deploy new brochure site

--- _static/js/custom.js
+++ _static/js/custom.js
@@ -1,1 +1,10 @@
-../../stable/_static/js/custom.js
+if (location.protocol == "http:") {
+  if (
+    !window.location.href.includes("localhost") &&
+    !window.location.href.includes("127.0.0.1")
+  ) {
+    location.href =
+      "https:" +
+      window.location.href.substring(window.location.protocol.length);
+  }
+}
