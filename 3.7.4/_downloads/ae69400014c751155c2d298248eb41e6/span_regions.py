659cfcc54e79d97991bce60ac9158270845355e7
Elliott Sales de Andrade <quantum.analyst@gmail.com>
2023-11-18 04:21:49 -0500

    Upload docs for v3.7.4

---
+++ 3.7.4/_downloads/ae69400014c751155c2d298248eb41e6/span_regions.py
@@ -0,0 +1,32 @@
+"""
+==========================================================
+Shade regions defined by a logical mask using fill_between
+==========================================================
+"""
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+t = np.arange(0.0, 2, 0.01)
+s = np.sin(2*np.pi*t)
+
+fig, ax = plt.subplots()
+
+ax.plot(t, s, color='black')
+ax.axhline(0, color='black')
+
+ax.fill_between(t, 1, where=s > 0, facecolor='green', alpha=.5)
+ax.fill_between(t, -1, where=s < 0, facecolor='red', alpha=.5)
+
+plt.show()
+
+
+#############################################################################
+#
+# .. admonition:: References
+#
+#    The use of the following functions, methods, classes and modules is shown
+#    in this example:
+#
+#    - `matplotlib.axes.Axes.fill_between`
---
+++ 3.7.4/_sources/users/index.rst.txt
@@ -0,0 +1,41 @@
+.. _users-guide-index:
+
+.. redirect-from:: /contents
+
+
+###########
+Users guide
+###########
+
+General
+#######
+
+.. toctree::
+   :maxdepth: 2
+
+   getting_started/index.rst
+   installing/index.rst
+   explain/index.rst
+   faq/index.rst
+   resources/index.rst
+
+Tutorials and examples
+######################
+
+.. toctree::
+   :maxdepth: 1
+
+   ../plot_types/index.rst
+   ../tutorials/index.rst
+   ../gallery/index.rst
+
+Reference
+#########
+
+.. toctree::
+   :maxdepth: 2
+
+   ../api/index.rst
+   ../devel/index.rst
+   project/index.rst
+   release_notes.rst
